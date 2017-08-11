cc94feab6509b083ef7e85dc451aebb9a7fc36b5
raphaeltony <30729856+raphaeltony@users.noreply.github.com>
2017-08-11 17:06:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,80 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+
+var articleOne = {
+    
+    title : 'Article One by Ralph',
+    heading : 'Article One',
+    date : '11 Aug 2017',
+    content : `
+                <p>
+                    This is my first paragraph.Yada yadaYada yadaYada yadaYada yadaYada yadaYada yadaYada yadaYada yadaYada yadaYada yadaYada yadaYada yada
+                    Yada yada
+                    yada
+                </p>
+                
+                <p>
+                    This is my second paragraph
+                    Bleh bleh
+                    bleh
+                </p>
+                
+                <p>
+                    This is my third paragraph
+                    I dont know any other word
+                </p>`
+    
+};
+
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+var htmlTemplate `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+        <div class="container">
+            <div>
+                <a href='/'>HOME</a>
+            </div>
+            <hr/>
+            
+            <h3>
+                ${heading}
+            </h3>
+            
+            <div>
+                ${date}
+            </div>
+            
+            <div>
+                ${content}
+            </div>
+        </div>
+    </body>
+        
+    
+    
+</html>
+
+`;
+return htmlTemplate;
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -18,7 +92,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article1',function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+    res.send(createTemplate(articleOne));
 });
 app.get('/article2',function (req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article2.html'));

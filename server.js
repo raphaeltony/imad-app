a4ba596280fb6828d4406ef0e5a003333631eccb
raphaeltony <30729856+raphaeltony@users.noreply.github.com>
2017-08-11 17:16:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,40 +38,40 @@ function createTemplate(data){
     var date = data.date;
     var content = data.content;
     
-var htmlTemplate `
-<html>
-    <head>
-        <title>
-            ${title}
-        </title>
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    
-    <body>
-        <div class="container">
-            <div>
-                <a href='/'>HOME</a>
+    var htmlTemplate `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width,initial-scale=1">
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        
+        <body>
+            <div class="container">
+                <div>
+                    <a href='/'>HOME</a>
+                </div>
+                <hr/>
+                
+                <h3>
+                    ${heading}
+                </h3>
+                
+                <div>
+                    ${date}
+                </div>
+                
+                <div>
+                    ${content}
+                </div>
             </div>
-            <hr/>
-            
-            <h3>
-                ${heading}
-            </h3>
-            
-            <div>
-                ${date}
-            </div>
-            
-            <div>
-                ${content}
-            </div>
-        </div>
-    </body>
-    
-</html>
-`;
-return htmlTemplate;
+        </body>
+        
+    </html>
+    `;
+    return htmlTemplate;
 }
 
 

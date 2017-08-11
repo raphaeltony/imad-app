24035cf7b6ba29ed0e815d3c989fc1debf49eac8
raphaeltony <30729856+raphaeltony@users.noreply.github.com>
2017-08-11 17:40:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,6 +111,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/:articleName',function (req,res){
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 

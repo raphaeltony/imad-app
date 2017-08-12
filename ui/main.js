c1bfe129c67fcf967e02b8e2f541d9f363e3b97d
raphaeltony <30729856+raphaeltony@users.noreply.github.com>
2017-08-12 16:58:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,6 @@ img.onclick = function(){
 var marginLeft = 0;
 
 function moveRight(){
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }

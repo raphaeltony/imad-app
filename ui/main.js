77cfa01821bf03286850daeb27e0b7f5396a4bac
raphaeltony <30729856+raphaeltony@users.noreply.github.com>
2017-08-13 16:05:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,13 @@ function moveRight(){
     marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }
+
+//Button Count
+var counter = 0;
+var button = document.getElementById('counter');
+button.onclick = function(){
+    counter += 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+}

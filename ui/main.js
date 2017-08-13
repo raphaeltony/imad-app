4e409c7af2101a2c17901bc65253733aecc9bb00
raphaeltony <30729856+raphaeltony@users.noreply.github.com>
2017-08-13 16:25:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,13 +13,42 @@ function moveRight(){
     marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }
+/////////////////////////////////////////////////////////////////////////////
 
 //Button Count
-var counter = 0;
 var button = document.getElementById('counter');
+
 button.onclick = function(){
-    counter += 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //make a request object
+var request = new XMLHttpRequest();
+
+//Capture the response:
+request.onreadystatechange = function(){    //if request changed :
+    if(request.readyState == XMLHttpResponse.DONE){
+        if(request.status == 200){ //If request was successful
+           var counter = request.responseText; 
+           var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+};
+
+//Make and send the request:
+request.open('GET','http://prince4raphael.imad.hasura-app.io/counter',true);
+request.send(null);
     
-}
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+

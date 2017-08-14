aad8930e0626c07517a1e0843218d9ae9c5b22be
raphaeltony <30729856+raphaeltony@users.noreply.github.com>
2017-08-14 17:33:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -68,7 +68,7 @@ submit.onclick = function(){
 //Make and send the request:
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
-request.open('GET','http://prince4raphael.imad.hasura-app.io/submit-name'+name,true);
+request.open('GET','http://prince4raphael.imad.hasura-app.io/submit-name?name='+name,true);
 request.send(null);
     
 };

436ceeb85405344b39bbadaa5a6aceea070328b1
raphaeltony <30729856+raphaeltony@users.noreply.github.com>
2017-08-14 16:58:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,6 +125,16 @@ app.get('/:articleName',function (req,res){
     res.send(createTemplate(articles[articleName]));
 });
 
+var names = [];
+app.get('/submit-name/:name',function(req,res){
+    var name = req.params.name; //Get name from request
+    names.push(name);
+    //JSON - Javascript Object Notation
+    //Helps converts js objects into strings
+    res.send(JSON.stringify(names));
+    
+});
+
 
 
 // Do not change port, otherwise your app won't run on IMAD servers

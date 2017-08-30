b10ef6f38e0fd340915cf04ae25d20b56ffd031f
raphaeltony <30729856+raphaeltony@users.noreply.github.com>
2017-08-30 22:54:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -146,6 +146,35 @@ app.post('/create-user',function(req,res){
    });
 });
 
+//Logging Users:
+app.post('/login',function(req,res){
+    var username = req.body.username;
+   var password = req.body.password;
+   
+   var dbString = hash(password,salt);
+   pool.query('SELECT * FROM "users" WHERE username=$1',[username],function(err,result){
+       if(err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length===0){
+                res.send(403).send("Invalid username/password");
+            }else{
+                //match the password
+                var dbString = result.rows[0].password;
+                var salt = dbString.split('$')[2];
+                var hashedPassword = hash(password,salt);
+                
+                if(hashedPassword===dbString){
+                    res.send('credentials correct !');
+                }else{
+                    res.send(403).send("Invalid username/password");
+                }
+            }
+            
+        }
+   });
+});
+
 
 
 

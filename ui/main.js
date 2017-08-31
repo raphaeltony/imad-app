be429e3149680d7d3bb0ff8a1830e73adfb011ea
raphaeltony <30729856+raphaeltony@users.noreply.github.com>
2017-08-31 13:39:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -73,6 +73,35 @@ request.send(null);
     
 };
 
+//submit username/password to login:
+var login = document.getElementById('login_btn');
+
+login.onclick = function(){
+    //Make request to server and send name
+    var request = new XMLHttpRequest();
+
+    //Capture the response:
+    request.onreadystatechange = function(){    //if request changed :
+        if(request.readyState == XMLHttpRequest.DONE){
+            if(request.status == 200){ //If request was successful
+                console.log("user logged in!")
+                alert('Logged in successfully !')
+                }
+            }
+        }
+    };
+
+//Make and send the request:
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST','http://prince4raphael.imad.hasura-app.io/login',true);
+request.send(JSON.stringify({username:username,password:password}));
+    
+};
+
+
 
 
 

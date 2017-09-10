085b829e257ce356d336afe1c13330020a181410
raphaeltony <30729856+raphaeltony@users.noreply.github.com>
2017-09-11 00:50:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,8 +96,8 @@ app.get('/counter', function (req,res) {
     res.send(counter.toString());
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname,'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
